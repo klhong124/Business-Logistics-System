42332ec3f19af7c0508cd130f6dd42a07c25e9a6
Ryan B <klhong124@gmail.com>
2019-03-07 11:19:20 +0800

    upload

--- resources/views/pages/confirm-invoice.blade.php
+++ resources/views/pages/confirm-invoice.blade.php
@@ -12,88 +12,93 @@
       <div class="row">
         {{-- right hand side --}}
         <div class="col-md-4 order-md-2 mb-4">
-          <h4 class="d-flex justify-content-between align-items-center mb-3">
-            <span class="text-muted">QR Code</span>
-          </h4>
-          <p>Please scan the code below to confirm the order</p>
-          <ul class="list-group mb-3">
-            <?php
-              echo QRCode::url('http://'.$_SERVER['REMOTE_ADDR'].':8080/complete_invoice'.'/'.$data->invoice_id)
-              // echo QRCode::url('http://ls27.asuscomm.com:8080/complete_invoice/60')
-              ->setSize(8)
-              ->setMargin(2)
-              ->svg();
-            ?>
-          </ul>
-        </div>
-        {{-- left hand side --}}
-        <div class="col-md-8 order-md-1">
-          <h4 class="mb-3">Receiver Details</h4>
-            <div class="row">
-              <div class="col-md-6 mb-3">
-                <label>Name</label>
-                <input type="text" disabled class="form-control" value="{{$data->receiver_name}}">
-              </div>
-            </div>
-
-            <div class="mb-3">
-              <label>Contact</label>
-              <div class="input-group">
-                <input type="text" disabled class="form-control" value="{{$data->contact}}">
-              </div>
-            </div>
-
-            <div class="mb-3">
-              <label for="address">Address</label>
-              <input type="text" disabled class="form-control" value="{{$data->address}}">
-            </div>
-            {{-- placeholder --}}
-            <hr class="mb-4"> 
-
-            <div class="row">
-              <div class="col-md-6 mb-3">
-                <h4 class="mb-3">Shipper</h4>
-                  <ul class="list-group mb-3">
-                      <li class="list-group-item d-flex justify-content-between lh-condensed">
-                        <small>Name: </small>
-                        <div>
-                          <h6 class="my-0">{{$data->name}}</h6>
-                        </div>
-                      </li>
-                      <li class="list-group-item d-flex justify-content-between lh-condensed">
-                        <small>Email: </small>
-                        <div>
-                          <h6 class="my-0">{{$data->email}}</h6>
-                        </div>
-                      </li>
-                      <li class="list-group-item d-flex justify-content-between lh-condensed">
-                          <small>URL: </small>
-                          <div>
-                            <h6 class="my-0">{{$data->url}}</h6>
-                          </div>
-                      </li>
+          <div class="card" style="width: 18rem;">
+              <div class="card-body">
+                <h5 class="card-title">QR Code</h5>
+                <p class="card-text">Please scan the code below to confirm the order</p>
+                <ul class="list-group mb-2">
+                    <?php
+                      echo QRCode::url('http://'.$_SERVER['REMOTE_ADDR'].':8080/complete_invoice'.'/'.$data->invoice_id)
+                      // echo QRCode::url('http://ls27.asuscomm.com:8080/complete_invoice/60')
+                      ->setSize(8)
+                      ->setMargin(2)
+                      ->svg();
+                    ?>
                   </ul>
               </div>
-              <div class="col-md-6 mb-3">
-                  <h4 class="mb-3">Goods Information</h4>
+            </div>
+        </div>
+
+        {{-- left hand side --}}
+        <div class="card-body">
+          <div class="col-md-8 order-md-1">
+            <h4 class="mb-3">Receiver Details</h4>
+              <div class="row">
+                <div class="col-md-6 mb-3">
+                  <label>Name</label>
+                  <input type="text" disabled class="form-control" value="{{$data->receiver_name}}">
+                </div>
+              </div>
+
+              <div class="mb-3">
+                <label>Contact</label>
+                <div class="input-group">
+                  <input type="text" disabled class="form-control" value="{{$data->contact}}">
+                </div>
+              </div>
+
+              <div class="mb-3">
+                <label for="address">Address</label>
+                <input type="text" disabled class="form-control" value="{{$data->address}}">
+              </div>
+              {{-- placeholder --}}
+              <hr class="mb-4"> 
+
+              <div class="row">
+                <div class="col-md-6 mb-3">
+                  <h4 class="mb-3">Shipper</h4>
                     <ul class="list-group mb-3">
                         <li class="list-group-item d-flex justify-content-between lh-condensed">
-                          <small>Quantity: </small>
+                          <small>Name: </small>
                           <div>
-                            <h6 class="my-0">{{$data->quantity}}</h6>
+                            <h6 class="my-0">{{$data->name}}</h6>
                           </div>
                         </li>
                         <li class="list-group-item d-flex justify-content-between lh-condensed">
-                          <small>Net Weight: </small>
+                          <small>Email: </small>
                           <div>
-                            <h6 class="my-0">{{$data->weight}}</h6>
+                            <h6 class="my-0">{{$data->email}}</h6>
                           </div>
                         </li>
-                      </ul>
+                        <li class="list-group-item d-flex justify-content-between lh-condensed">
+                            <small>URL: </small>
+                            <div>
+                              <h6 class="my-0">{{$data->url}}</h6>
+                            </div>
+                        </li>
+                    </ul>
+                </div>
+                <div class="col-md-6 mb-3">
+                    <h4 class="mb-3">Goods Information</h4>
+                      <ul class="list-group mb-3">
+                          <li class="list-group-item d-flex justify-content-between lh-condensed">
+                            <small>Quantity: </small>
+                            <div>
+                              <h6 class="my-0">{{$data->quantity}}</h6>
+                            </div>
+                          </li>
+                          <li class="list-group-item d-flex justify-content-between lh-condensed">
+                            <small>Net Weight: </small>
+                            <div>
+                              <h6 class="my-0">{{$data->weight}}</h6>
+                            </div>
+                          </li>
+                        </ul>
+                </div>
               </div>
-            </div>
-            <hr class="mb-4">
-            <button class="btn btn-primary btn-lg btn-block" type="submit"><i class="fas fa-print"></i> Print Invoice</button>
+              <hr class="mb-4">
+              <button class="btn btn-primary btn-lg btn-block" type="submit"><i class="fas fa-print"></i> Print Invoice</button>
+          </div>
         </div>
       </div>
     </div>
--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -12,6 +12,10 @@
         <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
 
         <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.2/css/all.css" integrity="sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr" crossorigin="anonymous">
+        <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+        <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+
         <!-- Styles -->
         <style>
             html, body {
@@ -48,7 +52,7 @@
             }
 
             .title {
-                font-size: 84px;
+                font-size: 8vw;
             }
 
             .links > a {
@@ -62,7 +66,7 @@
             .link > a {
                 color: #636b6f;
                 padding: 0 25px;
-                font-size: 16px;
+                font-size: 2vw;
                 font-weight: 600;
                 letter-spacing: .1rem;
                 text-decoration: none;
@@ -70,6 +74,7 @@
             }
 
             .m-b-md {
+                margin-top: 20%;
                 margin-bottom: 30px;
             }
 
@@ -96,48 +101,71 @@
         </style>
     </head>
     <body>
-        <div class="flex-center position-ref full-height">
-        <div class="top-right links">
-        <a href="{{ url('/') }}/csv-upload-page">Upload a CSV</a>
-        <a href="{{ url('/') }}/service">Services</a>
-        <a href="{{ url('/') }}/help">Help Center</a>
-        <a href="{{ url('/') }}/about-us">About Us</a>
-            @if (Route::has('login'))
-                    @auth
-                        <a href="{{ url('/') }}/dashboard">Dashboard</a>
-                        <a href="{{ url('/') }}/logout"> logout </a>
-                    @else
-                        <a href="{{ route('login') }}">Login</a>
-                        @if (Route::has('register'))
-                            <a href="{{ route('register') }}">Register</a>
+        <nav class="navbar navbar-expand-lg navbar-light bg-light" style="box-shadow: -2px -2px 16px rgba(0, 0, 0, .5);}">
+                <a class="navbar-brand" href="{{url('/')}}">曹操速遞</a>
+                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                
+                <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                    <ul class="navbar-nav mr-auto">
+                    <li class="nav-item">
+                        <a class="nav-link" href="{{ url('/') }}/csv-upload-page">Upload a CSV</a>
+                    </li>
+                    {{-- <li class="nav-item">
+                        <a class="nav-link" href="#">Link</a>
+                    </li> --}}
+                    <li class="nav-item dropdown">
+                        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                        Advanced
+                        </a>
+                        <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                            <a class="dropdown-item" href="{{ url('/') }}/service">Services</a>
+                            <a class="dropdown-item" href="{{ url('/') }}/help">Help Center</a>
+                            <a class="dropdown-item" href="{{ url('/') }}/about-us">About Us</a>
+                            {{-- <div class="dropdown-divider"></div> --}}
+                        </div>
+                    </li>
+                    </ul>
+                    <div class="form-inline my-2 my-lg-0">
+                        @if (Route::has('login'))
+                            @auth
+                                <a class="nav-link" href="{{ url('/') }}/dashboard">Dashboard</a>
+                                <a class="nav-link" href="{{ url('/') }}/logout"> logout </a>
+                            @else
+                                <a class="nav-link" href="{{ route('login') }}">Login</a>
+                                @if (Route::has('register'))
+                                    <a class="nav-link" href="{{ route('register') }}">Register</a>
+                                @endif
+                            @endauth
                         @endif
-                    @endauth
-            @endif
-            </div>
-            <div class="content">
-                <div class="title m-b-md">
-                    曹操速遞
-                </div>
-                <div class="link">
-                    <a href="{{ url('/') }}/service">Track & Trace</a>
-                    <a href="{{ url('/') }}/help">Shipping</a>
-                </div>
-            </div>
-            <div class="popupbox">
-                <form class="box" action="/query" method="get">
-                    Enter your invoice code: <br>
-                    <div class="input-group">
-                        <input type="text" class="form-control" name="q" placeholder="Invoice Code Search">
-                        <button type="button" class="btn btn-outline-primary"><i class="fas fa-search"></i></button>
                     </div>
-                </form> 
-                <!-- <form class="box" action="/uploadCSV" method="post" enctype="multipart/form-data">
-                    @csrf
-                    Select image to upload:
-                    <input type="file" name="fileToUpload" id="fileToUpload">
-                    <input type="submit" value="Upload Image" name="submit">
-                </form> -->
+                </div>
+        </nav>
+        <div class="container content">
+            <div class="row">
+                <div class="col">
+
+                    <div class="title m-b-md">
+                        曹操速遞
+                    </div>
+                    <div class="link">
+                        <a href="{{ url('/') }}/service">Track & Trace</a>
+                        <a href="{{ url('/') }}/help">Shipping</a>
+                    </div>
+                </div>
             </div>
         </div>
+        <footer class="my-5 pt-5 text-muted text-center text-small">
+                <div class="popupbox">
+                    <form class="box" action="/query" method="get">
+                        Enter your invoice code: <br>
+                        <div class="input-group">
+                            <input type="text" class="form-control" name="q" placeholder="Invoice Code Search">
+                            <button type="button" class="btn btn-outline-primary"><i class="fas fa-search"></i></button>
+                        </div>
+                    </form> 
+                </div>
+        </footer>
     </body>
 </html>
